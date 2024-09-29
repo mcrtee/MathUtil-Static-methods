86dc5ffebbff5b9c9e204eded3110a529797737d
mcrtee <meerim.mazaripova@alatoo.edu.kg>
2024-09-29 14:22:29 +0600

    Creating static methods

---
+++ MathUtil.java
@@ -0,0 +1,47 @@
+public class MathUtil {
+    public static boolean isPrime(int n) {
+        if (n<= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    public static int lcm(int a, int b) {
+        return (a * b) / gcd(a, b);
+    }
+
+    public static int fibonacci(int n) {
+        if (n <= 1) return n;
+        int a = 0, b = 1;
+        for (int i = 2; i <= n; i++) {
+            int next = a + b;
+            a = b;
+            b = next;
+        }
+        return b;
+    }
+
+    public static int factorial(int n) {
+        if (n == 0) return 1;
+        int result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+
+}
